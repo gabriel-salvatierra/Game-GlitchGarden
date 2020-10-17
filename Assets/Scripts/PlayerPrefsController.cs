43c6743d44e9a0cbd104bf6384f0ef5444d104b2
angelsalvatierra <angel.salvatierra@globallogic.com>
2020-10-17 14:16:35 -0300

    Course Complete - Needs Tuning

--- Assets/Scripts/PlayerPrefsController.cs
+++ Assets/Scripts/PlayerPrefsController.cs
@@ -10,6 +10,8 @@ public class PlayerPrefsController : MonoBehaviour {
     const float MIN_VOLUME = 0f;
     const float MAX_VOLUME = 1f;
 
+    const float MIN_DIFFICULTY = 0f;
+    const float MAX_DIFFICULTY = 2f;
 
     public static void SetMasterVolume(float volume) {
         if (volume >= MIN_VOLUME && volume <= MAX_VOLUME) {
@@ -24,6 +26,17 @@ public class PlayerPrefsController : MonoBehaviour {
         return PlayerPrefs.GetFloat(MASTER_VOLUME_KEY);
     }
 
-    // public static void SetDifficulty(string difficulty);
+    public static void SetDifficulty(float difficulty) {
+        if (difficulty >= MIN_DIFFICULTY && difficulty <= MAX_DIFFICULTY) {
+            PlayerPrefs.SetFloat(DIFFICULTY_KEY, difficulty);
+        } else {
+            Debug.LogError("Difficulty setting is not in range");
+        }
+    }
+
+    public static float GetDifficulty() {
+        return PlayerPrefs.GetFloat(DIFFICULTY_KEY);
+    }
+
 
 }
